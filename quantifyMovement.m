46ca291781970b5b54048d5cd739312cf5149dfe
tomasalvarezm <104563689+tomasalvarezm@users.noreply.github.com>
2024-06-24 11:06:41 +0200

    Update quantifyMovement.m

--- quantifyMovement.m
+++ quantifyMovement.m
@@ -7,14 +7,14 @@ function movement = quantifyMovement(data, fs)
     window_size = 10;
     numSamples = length(data);
     numWindows = floor(numSamples / (window_size * fs)); % number of samples in a window: windowSize * sampling freq
-    energy = zeros(1, numWindows);
+    energy = zeros(numWindows, 1);
 
     % energy of the signal
     for i = 1:numWindows
         startIndex = (i-1) * window_size * fs + 1;
         endIndex = i * window_size * fs;
         windowData = data(startIndex:endIndex);
-        energy(i) = sum(windowData.^2);
+        energy(i) = sqrt(sum(windowData.^2))/(window_size * fs);
     end
 
     movement = energy;
