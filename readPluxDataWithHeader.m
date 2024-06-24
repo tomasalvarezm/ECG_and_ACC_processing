02ed4f285e5ce24d35c35de473f96e6b0436d1e8
tomasalvarezm <104563689+tomasalvarezm@users.noreply.github.com>
2024-06-24 11:07:19 +0200

    Update readPluxDataWithHeader.m

--- readPluxDataWithHeader.m
+++ readPluxDataWithHeader.m
@@ -3,11 +3,12 @@
 % the ECG data to millivolts. The function returns the ECG data and the accelerometer 
 % data (X, Y, Z axes) as separate output variables.
 
-function [ECG_mv, accX, accY, accZ] = readPluxDataWithHeader(fileName, ecgChannel, accChannels)
+function [ECG_mv, accX, accY, accZ, accX_2, accY_2, accZ_2] = readPluxDataWithHeader(fileName, ecgChannel, accChannels, accChannels_2)
     % Read data from the first file
     ecg = importdata(fileName);
     data_ecg = ecg.data(:, ecgChannel);
     data_acc = ecg.data(:, accChannels);
+    data_acc_2 = ecg.data(:, accChannels_2);
     
     % Convert ECG data to millivolts
     %https://support.pluxbiosignals.com/knowledge-base/transfer-functions-converting-digital-sensor-data-into-physical-units/
@@ -18,5 +19,9 @@ function [ECG_mv, accX, accY, accZ] = readPluxDataWithHeader(fileName, ecgChanne
     accX = data_acc(:, 3);
     accY = data_acc(:, 2);
     accZ = data_acc(:, 1);
+
+    accX_2 = data_acc_2(:, 1);
+    accY_2 = data_acc_2(:, 2);
+    accZ_2 = data_acc_2(:, 3);
     
 end
